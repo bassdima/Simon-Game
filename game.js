c11658c01f486f23095b2f86b1e200becb2fb3c4
Dmytro Yaroshenko <102763338+bassdima@users.noreply.github.com>
2022-12-03 00:49:03 +0200

    Simon-game

---
+++ game.js
@@ -0,0 +1,88 @@
+const values = {
+  level: 0,
+  gameChosenColour: [],
+  userClickedColour: [],
+  buttonColours: ['red', 'blue', 'green', 'yellow']
+}
+
+const controlElements = {
+  title: document.querySelector('[data-control="title"]'),
+  buttons: document.querySelectorAll('[data-control="btn"]')
+}
+
+const playSound = (name) => {
+  const audio = new Audio('sounds/' + name + '.mp3');
+
+  audio.play();
+}
+
+const switchCssClass = (element, cssClass) => {
+  const elem = element.classList;
+
+  elem.add(cssClass);
+  setTimeout(() => {
+    elem.remove(cssClass);
+  }, 100);
+}
+
+const showGameChoosigColor = (colors, cssClass) => {
+  colors.forEach((elem, i) => {
+    setTimeout(() => {
+      switchCssClass(document.getElementById(elem), cssClass);
+      playSound(elem);
+    }, i * 500)
+  })
+}
+
+const setLevel = () => {
+  ++values.level;
+  controlElements.title.innerHTML = 'Level ' + values.level;
+}
+
+const startNextSequence = () => {
+  const { buttonColours, gameChosenColour } = values;
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColour = buttonColours[randomNumber];
+  const activeBtnClass = 'active-btn-js';
+
+  gameChosenColour.push(randomChosenColour);
+  showGameChoosigColor(gameChosenColour, activeBtnClass)
+  setLevel();
+}
+
+const startTheGame = () => {
+    startNextSequence();
+    window.removeEventListener('click', startTheGame);
+}
+
+const checkAnswer = () => {
+  const { userClickedColour, gameChosenColour } = values;
+  const index = userClickedColour.length - 1;
+
+  if (userClickedColour[index] !== gameChosenColour[index]) {
+    playSound('wrong');
+    switchCssClass(document.body, 'game-over');
+    controlElements.title.innerHTML = "GAME OVER<br/>Press title to Restart";
+    controlElements.title.addEventListener('click', () => window.location.reload());
+  }
+
+  if (userClickedColour.toString() === gameChosenColour.toString()) {
+    setTimeout(() => {
+      startNextSequence();
+    }, 1000);
+    userClickedColour.length = 0;
+  }
+}
+
+controlElements.buttons.forEach((elem) => {
+  elem.addEventListener('click', (event) => {
+    const { id } = event.target;
+
+    values.userClickedColour.push(id);
+    playSound(id);
+    switchCssClass(event.target, 'pressed');
+    checkAnswer();
+  });
+});
+
+window.addEventListener('click', startTheGame);
